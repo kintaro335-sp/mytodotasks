bff4b953950396c2e5d3601071c1337c15607459
kintaro335-sp <kintaroponce.uni@gmail.com>
2022-01-02 12:54:08 -0600

    arreglo de los types

---
+++ server/index.d.ts
@@ -0,0 +1,49 @@
+// types
+
+// request
+type req = {
+  params: any;
+  query: any;
+  body: any;
+};
+// response
+type res = {
+  json: Function;
+  status: Function;
+  send: Function;
+};
+// callback route
+type callbakRoute = (req: req, res: res) => void;
+// route
+type route = (route: string, callback: callbakRoute) => void;
+
+type appT = {
+  listen: Function;
+  use: Function;
+  get: route;
+  post: route;
+  put: route;
+  delete: route;
+};
+
+type routerT = {
+  use: Function;
+  get: route;
+  post: route;
+  put: route;
+  delete: route;
+};
+
+type taskT = {
+  id: string | undefined;
+  done: boolean;
+  nombre: string;
+  descripcion: string;
+};
+
+type response = {
+  status: string;
+  code : number;
+  message: string;
+  data: string | Array<taskT> | taskT;
+}
--- server/index.ts
+++ server/index.ts
@@ -1,50 +1,15 @@
-// types
-
-// request
-type req = {
-  params: any;
-  query: any;
-  body: any;
-};
-// response
-type res = {
-  json: Function;
-  status: Function;
-  send: Function;
-};
-// callback route
-type callbakRoute = (req: req, res: res) => void;
-// route
-type route = (route: string, callback: callbakRoute) => void;
-
-type appT = {
-  listen: Function;
-  use: Function;
-  get: route;
-  post: route;
-  put: route;
-  delete: route;
-};
-
-type routerT = {
-  use: Function;
-  get: route;
-  post: route;
-  put: route;
-  delete: route;
-};
-
+/// <reference path="index.d.ts" />
 const dotenv = require('dotenv');
 dotenv.config();
 // app
 const express = require('express');
-const app = express();
+const app: appT = express();
 const port = process.env.SERVER_PORT || 4000;
 const routes = require('./app/routes');
 
 app.use(express.json());
 
-app.get('/', (req: any, res: any) => {
+app.get('/', (req, res) => {
   res.json({ message: 'hola mundo' });
 });
 
