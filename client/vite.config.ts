0b49fb7737e8e4b53ab53fe82ffc923699397e1f
kintaro335-sp <kintaroponce.uni@gmail.com>
2022-03-25 20:33:27 -0600

    chunk splitting

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { dependencies } from './package.json';
+import react from '@vitejs/plugin-react';
+
+function renderChunks(deps: Record<string, string>) {
+  let chunks = {};
+  Object.keys(deps).forEach((key) => {
+    if (['react', 'react-router-dom', 'react-dom'].includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'build'
+    outDir: 'build',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-router-dom', 'react-dom'],
+          ...renderChunks(dependencies)
+        }
+      }
+    }
   },
   plugins: [react()]
-})
+});
